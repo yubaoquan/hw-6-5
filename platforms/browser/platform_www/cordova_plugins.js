a4147c1cdd0125e42d14676896a008295e77041b
yubaoquan <yuyuyubq@163.com>
2021-09-15 20:53:17 +0800

    add whitelist plugin

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-geolocation": "4.1.0"
+    "cordova-plugin-geolocation": "4.1.0",
+    "cordova-plugin-whitelist": "1.3.5"
 }
 // BOTTOM OF METADATA
 });
